c6ed3fa94440db6dd957e7b769ec2db53196dfe8
MADFutcher <mikalob247@gmail.com>
2021-03-06 15:45:55 +0100

    removed old list group

--- src/components/cards/PlantCard.js
+++ src/components/cards/PlantCard.js
@@ -49,40 +49,6 @@ export default function PlantCard(props) {
                                 
                             })
                         }
-                        {/* {link && 
-                            plantInfo.map((plant,index, plantInfo)=>{
-                                if(plantInfo.length===1){
-                                    return (
-                                        <ListGroup.Item as='button' key={plant._id}  style={{backgroundColor:'#282c34'}} onClick={()=>{handleOnClick(plant._id)}}>{plant.name}</ListGroup.Item>
-                                    )
-                                }else if(plantInfo.length===2){
-                                    if(index===0){
-                                        return (
-                                            <ListGroup.Item as='button' key={plant._id} className="BorderTLR" style={{backgroundColor:'#282c34'}} onClick={()=>{handleOnClick(plant._id)}}>{plant.name}</ListGroup.Item>
-                                        )
-                                    }else{
-                                        return (
-                                            <ListGroup.Item as='button' key={plant._id} className="BorderBLR" style={{backgroundColor:'#282c34'}} onClick={()=>{handleOnClick(plant._id)}}>{plant.name}</ListGroup.Item>
-                                        )
-                                    }
-                                    
-                                }else if(index === 0 && plantInfo.length>2){
-                                    return (
-                                        <ListGroup.Item as='button' key={plant._id} className="BorderTLR" style={{backgroundColor:'#282c34', cursor:'pointer'}} onClick={()=>{handleOnClick(plant._id)}}>{plant.name}</ListGroup.Item>
-                                    )
-                                }else if(plantInfo.length>2 && index<plantInfo.length && index!=plantInfo.length-1){
-                                    return (
-                                        <ListGroup.Item as='button' key={plant._id} className="BorderLR" style={{backgroundColor:'#282c34'}} onClick={()=>{handleOnClick(plant._id)}}>{plant.name}</ListGroup.Item>
-                                    )
-                                    
-                                }else if(index===plantInfo.length-1){
-                                    return (
-                                        <ListGroup.Item as='button' key={plant._id} className="BorderBLR" style={{backgroundColor:'#282c34'}} onClick={()=>{handleOnClick(plant._id)}}>{plant.name}</ListGroup.Item>
-                                    )
-                                }
-                                
-                            })
-                        } */}
                     </ListGroup>
             </Card.Body>
         </Card>
